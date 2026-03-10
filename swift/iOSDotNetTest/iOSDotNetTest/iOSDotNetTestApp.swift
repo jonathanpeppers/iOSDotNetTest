f862e4c69c8e403d08fed80f18e25f55ca066e80
Jonathan Peppers <jonathan.peppers@microsoft.com>
2026-03-10 13:44:00 -0500

    New swift project

---
+++ swift/iOSDotNetTest/iOSDotNetTest/iOSDotNetTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDotNetTestApp.swift
+//  iOSDotNetTest
+//
+//  Created by Jonathan Peppers on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct iOSDotNetTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
